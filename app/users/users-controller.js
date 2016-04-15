7f2fa4f8a7706c9711cfe5ee6c6708a68f457da3
TanyaRan <tatyanaran@gmail.com>
2016-04-15 16:04:12 +0300

    Forms validation

--- app/users/users-controller.js
+++ app/users/users-controller.js
@@ -5,10 +5,10 @@ angular.module('issueTracking.users')
   .controller('UsersController', ['$scope',
     function UsersController($scope) {
       $scope.login = function (user) {
-        // console.log(user);
+         console.log(user);
       };
 
       $scope.register = function (user) {
-        // console.log(user);
+         console.log(user);
       };
     }]);
